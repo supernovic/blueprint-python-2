2289235f4fa0cfc1a04b87928b5f92e4b8393de9
Victor Chang <victorchang@Victors-MacBook-Pro.local>
2013-02-07 20:28:02 -0800

    adding scheduler.py and test txt

---
+++ scheduler.py
@@ -0,0 +1,9 @@
+import sys
+
+schedule = open(sys.argv[1])
+for line in schedule:
+	# p = Person(line)
+	# add p to a list of people
+	# sort the people
+	# run scheduling algorithm to an array[17]
+	print line
